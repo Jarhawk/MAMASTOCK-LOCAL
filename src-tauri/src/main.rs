7bae5986336d9cd40b54d56885e743805b195686
Jarhawk <dark_territory@msn.com>
2025-09-07 10:19:05 +0200

    Update main.rs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
-        .plugin(tauri_plugin_sql::init())
+        .plugin(tauri_plugin_sql:::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
