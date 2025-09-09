b9095d063ad91510d81e6c93a1a8a689781b620a
Jarhawk <dark_territory@msn.com>
2025-09-09 09:09:18 +0200

    up cargo

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,25 +1,28 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-use tauri::Manager;
-
+﻿#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 fn main() {
-  tauri::Builder::default()
-    // === Plugins côté Rust ===
-    // FS en DEV + PROD
-    .plugin(tauri_plugin_fs::init())
+    let mut builder = tauri::Builder::default();
 
-    // Log seulement en release (évite "logger already initialized")
+    // Plugins communs (dev + prod)
+    builder = builder
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_fs::init());
+
+    // Dev uniquement (évite les conflits de logger)
+    #[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(tauri_plugin_devtools::init());
+    }
+
+    // Prod uniquement (logger)
     #[cfg(not(debug_assertions))]
-    .plugin(tauri_plugin_log::Builder::default().build())
+    {
+        builder = builder.plugin(tauri_plugin_log::Builder::default().build());
+    }
 
-    .setup(|app| {
-      // DevTools seulement en DEV
-      #[cfg(debug_assertions)]
-      if let Some(w) = app.get_webview_window("main") {
-        w.open_devtools();
-      }
-      Ok(())
-    })
-    .run(tauri::generate_context!())
-    .expect("erreur au lancement de Tauri");
+    builder
+        .run(tauri::generate_context!())
+        .expect("erreur au lancement de Tauri");
 }
+
