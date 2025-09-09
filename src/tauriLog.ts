d84c8c9b7efa65ee30164958955ff9f7e07165b3
Jarhawk <dark_territory@msn.com>
2025-09-09 11:54:42 +0200

    tauri cargo

---
+++ src/tauriLog.ts
@@ -0,0 +1,31 @@
+export type LogApi = {
+  info: (...a: any[]) => any;
+  warn: (...a: any[]) => any;
+  error: (...a: any[]) => any;
+  debug?: (...a: any[]) => any;
+  trace?: (...a: any[]) => any;
+};
+
+let api: Partial<LogApi> | null = null;
+
+export async function initLog() {
+  if (import.meta.env.PROD) {
+    try {
+      api = await import("@tauri-apps/plugin-log") as any;
+    } catch (_) {
+      api = null;
+    }
+  }
+}
+
+function passToConsole(fn: keyof Console, ...a: any[]) {
+  (console[fn] ?? console.log)(...a);
+}
+
+export const log: LogApi = {
+  info:  (...a) => (api && (api as any).info)  ? (api as any).info(...a)  : passToConsole("info",  ...a),
+  warn:  (...a) => (api && (api as any).warn)  ? (api as any).warn(...a)  : passToConsole("warn",  ...a),
+  error: (...a) => (api && (api as any).error) ? (api as any).error(...a) : passToConsole("error", ...a),
+  debug: (...a) => (api && (api as any).debug) ? (api as any).debug(...a) : passToConsole("debug", ...a),
+  trace: (...a) => (api && (api as any).trace) ? (api as any).trace(...a) : passToConsole("trace", ...a),
+};
