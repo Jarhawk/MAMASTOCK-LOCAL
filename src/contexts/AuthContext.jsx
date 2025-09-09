64e521b2a8b2d68906764221efd01a12b7f0eada
Jarhawk <dark_territory@msn.com>
2025-09-09 15:17:14 +0200

    refactor: replace node crypto with web crypto

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,7 +1,7 @@
 import { createContext, useContext, useEffect, useState, useMemo } from 'react';
 import bcrypt from 'bcryptjs';
 import users from '@/db/users.json';
-import { randomBytes } from '@/crypto/random';
+import { randomBytes } from '/src/crypto/random';
 
 bcrypt.setRandomFallback(randomBytes);
 
