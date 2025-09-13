65eb348af6be2500d386f93d3bac4e1252cd3552
Jarhawk <dark_territory@msn.com>
2025-09-13 17:00:31 +0200

    refactor: simplify router

--- src/router.tsx
+++ src/router.tsx
@@ -1,26 +1,8 @@
-import React, { Suspense } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AppLayout from "@/layout/AppLayout";
-import Loading from "@/ui/Loading"; // spinner existant ou simple div
-import { buildAutoElements } from "./router.autogen";
+import routes from "@/router.autogen";
 
-const children = [
-  // ...tes routes statiques si tu en as
-  ...buildAutoElements()
-];
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <AppLayout />,
-    children,
-  },
-]);
+const router = createBrowserRouter([...routes]);
 
 export default function AppRouter() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <RouterProvider router={router} />
-    </Suspense>
-  );
+  return <RouterProvider router={router} />;
 }
