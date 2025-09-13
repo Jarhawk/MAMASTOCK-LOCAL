212c5a57befd58d793cfd5548b71052707be1a11
Jarhawk <dark_territory@msn.com>
2025-09-13 18:34:54 +0200

    feat: conditionally enable PWA plugin for Tauri

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA as pwa } from "vite-plugin-pwa";
 
-const isTauri = !!process.env.TAURI_PLATFORM;
+const isTauri =
+  !!process.env.TAURI_PLATFORM || process.env.VITE_TAURI === "1";
+
+const plugins = [
+  react(),
+  !isTauri &&
+    pwa({
+      registerType: "autoUpdate",
+    }),
+].filter(Boolean);
 
 export default defineConfig({
   base: isTauri ? "./" : "/",
-  plugins: [react()],
+  plugins,
   resolve: {
     alias: { "@": path.resolve(__dirname, "src") },
   },
